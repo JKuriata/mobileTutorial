b8ea78a60ac452a25e27a5eb5e114dde7990f209
JKuriata <JK271096@outlook.com>
2024-09-21 15:19:49 +0100

    enhancement: footer

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -5,6 +5,7 @@ export function Gallery() {
     <>
     <h1>Gallery page</h1>
     <GalleryComponent/>
+    <footer>None of these pictures are owned by me, I am simply using them for practice of web design.</footer>
     </>
   );
 }
