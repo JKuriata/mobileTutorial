e7c4cc0932fe80c772840916cf8ee3e9e6acddc5
JKuriata <JK271096@outlook.com>
2024-09-23 18:23:03 +0100

    feat: Add CSS to center contact form and arrange inputs in a column

--- src/Components/Contact/ContactComponent.jsx
+++ src/Components/Contact/ContactComponent.jsx
@@ -23,14 +23,16 @@ export const ContactComponent = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="contact-form">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   );
 };
