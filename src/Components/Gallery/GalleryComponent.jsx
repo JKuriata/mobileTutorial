7b3066271b39bb74410fc09fa47263e2c3a81171
JKuriata <JK271096@outlook.com>
2024-09-25 21:41:30 +0100

    refactor: Reduce function nesting in GalleryComponent

--- src/Components/Gallery/GalleryComponent.jsx
+++ src/Components/Gallery/GalleryComponent.jsx
@@ -3,57 +3,62 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import './Gallery.css';
 
+const loadImage = (module, path) => {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.src = module.default;
+        img.onload = () => {
+            resolve({
+                src: module.default,
+                alt: path.split('/').pop(),
+                width: img.width,
+                height: img.height
+            });
+        };
+    });
+};
+
+const loadImages = async (imageModules) => {
+    const imagePromises = Object.entries(imageModules).map(([path, module]) => 
+        module().then(m => loadImage(m, path))
+    );
+    return Promise.all(imagePromises);
+};
+
 export const GalleryComponent = () => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
         const imageModules = import.meta.glob('../../assets/*.{jpg,png,svg}');
-
-        const imagePromises = Object.keys(imageModules).map((path) =>
-            imageModules[path]().then((module) => {
-                const img = new Image();
-                img.src = module.default;
-                return new Promise((resolve) => {
-                    img.onload = () => {
-                        resolve({
-                            src: module.default,
-                            alt: path.split('/').pop(),  // Get the file name
-                            width: img.width,
-                            height: img.height
-                        });
-                    };
-                });
-            })
-        );
-
-        Promise.all(imagePromises).then((imageArray) => {
-            setImages(imageArray);
-        });
+        loadImages(imageModules).then(setImages);
     }, []);
 
     return (
         <div className="gallery">
             {images.map((image, index) => (
-                <div
-                    key={index}
-                    className={`gallery-item ${image.width > image.height ? 'landscape' : 'portrait'}`}
-                    style={{
-                        gridRowEnd: `span ${Math.ceil(image.height / image.width * 4)}`
-                    }}
-                >
-                    <LazyLoadImage
-                        src={image.src}
-                        alt={image.alt}
-                        effect="blur"
-                        wrapperClassName="lazy-load-image-wrapper"
-                        width="100%"
-                        height="100%"
-                    />
-                    <div className="overlay">
-                        <div className="title">{image.alt.split('.')[0]}</div>
-                    </div>
-                </div>
+                <GalleryItem key={index} image={image} />
             ))}
         </div>
     );
 };
+
+const GalleryItem = ({ image }) => (
+    <div
+        className={`gallery-item ${image.width > image.height ? 'landscape' : 'portrait'}`}
+        style={{
+            gridRowEnd: `span ${Math.ceil(image.height / image.width * 4)}`
+        }}
+    >
+        <LazyLoadImage
+            src={image.src}
+            alt={image.alt}
+            effect="blur"
+            wrapperClassName="lazy-load-image-wrapper"
+            width="100%"
+            height="100%"
+        />
+        <div className="overlay">
+            <div className="title">{image.alt.split('.')[0]}</div>
+        </div>
+    </div>
+);
