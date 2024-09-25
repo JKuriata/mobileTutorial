7fd0ccbd7e00dc4f10ffa47bc03913c89fede253
JKuriata <JK271096@outlook.com>
2024-09-25 21:16:29 +0100

    feat: Increase content wrapper width on larger screens

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -3,7 +3,6 @@ import { GalleryComponent } from "../Components/Gallery/GalleryComponent";
 export function Gallery() {
   return (
     <>
-    <h1>Gallery page</h1>
     <GalleryComponent/>
     <footer>None of these pictures are owned by me, I am simply using them for practice of web design.</footer>
     </>
